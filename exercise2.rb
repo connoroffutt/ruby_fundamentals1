130fd18e2e96b4e8e75ebe5706bb2ca3d0ea19b1
connoroffutt <connor.ferguson.offutt@gmail.com>
2016-10-18 13:44:42 -0400

    Update exercise2.rb

--- exercise2.rb
+++ exercise2.rb
@@ -4,5 +4,5 @@ tip=(55.to_f * 3/20.to_f)
 puts "A good tip for $55 is $"+tip.to_s+"."
 # --> include interpolation in string via "#{$$$ * $$$}"
 puts "A really big number comes from multiplying 45628 by 7839, this number is exactly #{45628 * 7839}."
-# --> Value of below expression is true
+# --> Value of below expression is true / simplified = (true && false) || !(false) / more simplified = (true) or not(false) / = (true) or (true)
 puts (10 < 20 && 30 < 20) || !(10 == 11) 
